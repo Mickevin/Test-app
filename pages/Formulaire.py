2a359574a17627ee6acccc86e9868619fd5c70f2
Kévin Duranty <47926071+Mickevin@users.noreply.github.com>
2023-11-21 16:49:53 +0100

    Update Formulaire.py

--- pages/Formulaire.py
+++ pages/Formulaire.py
@@ -1,10 +1,13 @@
 import streamlit as st
+import datetime
 
 st.title("Formulaire de contact")
 
 with st.form('Formulaire de contact'):
     name = st.text_input('Tapez votre nom')
+    mail = st.text_input('Tapez votre mail')
     age = st.slider('Quel est votre age ?', 0, 130, 25)
+    date = st.date_input("Date de naissance", datetime.date(2019, 7, 6))
 
-    if st.form_submit_button('Send'):
-        st.write(name  + ", vous avez " + age + " ans.")
+    if st.form_submit_button('Envoyer'):
+        st.write(name  + " , vous avez " + str(age) + " ans.")
